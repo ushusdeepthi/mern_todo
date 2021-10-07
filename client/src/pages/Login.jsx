fa6c7a25210a1dd11f0444486322ed69fcf60636
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-07 15:28:39 +0200

    Add a login route and page in frontend

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,66 @@
+import React, {useState} from 'react'
+import {Link, useHistory} from 'react-router-dom'
+import { Container,AppBar,Typography, TextField, Button} from '@material-ui/core'
+import axios from "axios"
+
+export default function Login() {
+    const history = useHistory()
+    const apiUrl = "http://localhost:5000/api/login";
+    const [email,setEmail] =useState('')
+    const [password,setPassword] =useState('')
+    const handleSubmit = async()=>{
+        try{
+             if(email && password){
+                const credentials ={
+                   email,
+                   password
+                }
+                const user = await axios.post(apiUrl,credentials)
+                if(user){
+                    localStorage.setItem("user", JSON.stringify(user.data))
+                    history.push('/todolist')
+                }
+                else{
+                    console.log('no user')
+                }
+            }
+        }catch(err){
+
+        }
+        }
+    return (
+        <Container maxWidth="md">
+            <AppBar position="static">
+                <Typography variant="h2" align="center">TODO APP</Typography>
+            </AppBar>
+            <form  onSubmit={handleSubmit}>
+                <TextField
+                    label="Email"
+                    variant="filled"
+                    type="email"
+                    required
+                    fullWidth
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <TextField
+                    label="Password"
+                    variant="filled"
+                    type="password"
+                    required
+                    fullWidth
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <div>
+                    <Button type="submit" variant="contained" color="primary">
+                    Log in
+                    </Button>
+                    <p>Do not have an coount! 
+                        <Link to="/login"> Register </Link>
+                    </p>
+                </div>
+            </form>
+        </Container>
+    )
+}
