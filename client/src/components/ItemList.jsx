af869ec56d9b366db781febc03d90e949cb74f96
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-08 08:06:24 +0200

    Implement get request in Itemlist to fetch todo list from api

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -4,13 +4,22 @@ import { UserContext } from '../contexts/UserContext'
 
 export default function ItemList() {
     const apiUrl = "http://localhost:5000/api/items";
-    const [todos, setTodos] = useState([]); //state to store todoList
-    const {authHeader}=useContext(UserContext) // Token to be send along with header for authorisation
+    const [todos, setTodos] = useState([]);
+    const {authHeader}=useContext(UserContext)
 
     useEffect(()=>{
-        console.log('Axios request for get todo List')
+        getTodoList();
     },[])
 
+    const getTodoList =  async()=>{
+        try{
+            const list = await axios.get(apiUrl,{headers:authHeader()})
+            setTodos(list.data)
+            console.log(list.data)
+        }catch(err){
+            console.log(err)
+        }        
+    }
     return (
         <div>
             <h1>The list goes here</h1>
