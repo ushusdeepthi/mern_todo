0dfec6daa123dcf98ac6c0f3a1fd09d06867d72f
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-08 06:31:49 +0200

    Add structure to Todo Page

--- client/src/pages/Todo.jsx
+++ client/src/pages/Todo.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-
+import { Container, AppBar, Typography,Grow, Grid} from '@material-ui/core'
 
 
 export default function App() {
   return (
-         <div>
-           <h1>Todo List</h1>
-           <h1>create new todo</h1>
-         </div>
-
+          <Container maxWidth="lg">
+            <AppBar position="static">
+                <Typography variant="h2" align="center">TODO APP</Typography>
+            </AppBar>
+            <Grow in>
+              <Container>
+                <Grid container spacing={2}>
+                  <Grid item xs={8}>
+                    <h1>Here  goes the item List</h1>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <h1>Here  goes the item List</h1>
+                  </Grid>
+                </Grid>
+              </Container>
+            </Grow>
+          </Container>
   )
 }
