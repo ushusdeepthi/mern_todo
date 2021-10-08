0cdd393a54749f4eebcdac71e3b2b3a7d18a1b4b
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-08 18:37:41 +0200

    feat: Include form to add new item

---
+++ client/src/components/ItemForm.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useContext } from 'react'
+import axios from "axios"
+import {TextField, Button} from '@material-ui/core';
+import { UserContext } from '../contexts/UserContext'
+
+
+export default function ItemForm() {
+    const apiUrl = "http://localhost:5000/api/items";
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const {authHeader}=useContext(UserContext)
+
+    const handleSubmit= async(e)=>{
+        e.preventDefault();
+        try{
+            if(title && description){
+                const newItem ={
+                    "title": title,
+                    "body":description
+                }
+            return await axios.post(apiUrl, {headers:authHeader()},newItem)
+            }
+            else{
+                console.log('Title and description mandatory')
+            }
+        }catch(error){
+            console.log(error.message)
+        }
+
+        //Do an axios post request to the api
+    }
+    return (
+        <>
+            <h1>Add a new Note</h1>
+            <form autoComplete="off" onSubmit={handleSubmit}>
+                <TextField
+                    onChange = {(e)=>setTitle(e.target.value)}
+                    label="Title"
+                    variant="outlined"
+                    color="primary"
+                    fullWidth
+                    required
+                />
+                <TextField 
+                    onChange = {(e)=>setDescription(e.target.value)}
+                    label="Description"
+                    variant="outlined"
+                    color="primary"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    required
+                />
+                <Button 
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                >
+                    Add note
+                </Button>
+            </form>
+        </>
+    )
+}
