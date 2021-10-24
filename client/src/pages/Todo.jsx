e2b249d3061ac2556956c2d18909598e7d5bdf82
Deepthi Ushus <ushusdeepthi@gmail.com>
2021-10-24 14:56:06 +0200

    Add user info to appbar

--- client/src/pages/Todo.jsx
+++ client/src/pages/Todo.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { Container,AppBar,Typography,Toolbar,CssBaseline,Grow,Grid} from '@material-ui/core'
 import EventNoteIcon from '@material-ui/icons/EventNote';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import useStyles from '../styles/styles'
 import ItemList from '../components/ItemList'
 import ItemForm from '../components/ItemForm'
+import { UserContext } from '../contexts/UserContext'
 
 
 export default function App() {
       const classes = useStyles()
+      const {user}=useContext(UserContext)
   return (
     <>
     <CssBaseline />
@@ -15,6 +18,14 @@ export default function App() {
                 <Toolbar>
                     <EventNoteIcon className= {classes.icon} fontSize="large"/>
                     <Typography variant="h6">Plan IT</Typography>
+
+                    {user && 
+                      <>
+                        <Typography variant="h6">{user.name}</Typography>
+                        <ExitToAppIcon />
+                      </>  
+                      }
+
                 </Toolbar>
             </AppBar>
           <Container maxWidth="lg">
